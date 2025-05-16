35de2cab68340c798f955e22d5752c9d5b1c37d1
daralams <strongpeople006@gmail.com>
2025-05-16 23:24:27 +0700

    [add]: techstack section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../section/HeroSection'
+import TechStack from '../section/TechstackSection'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <TechStack/>
     </>
   )
 }
