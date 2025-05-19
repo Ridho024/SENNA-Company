a42bfe1896d861f230a54e5c5b6e36cc957d5343
Farizi Adam <92348092+fariziadam11@users.noreply.github.com>
2025-05-20 05:47:56 +0700

    Fix React DOM properties

--- src/section/Home/ContactUsSection.jsx
+++ src/section/Home/ContactUsSection.jsx
@@ -11,32 +11,32 @@ const ContactUsSection = () => {
           <p className="mb-4">If you have any questions or inquiries, feel free to reach out to us!</p>
           <div style={{ textAlign: "center"}} className="mb-5">
             <div style={{ width: "500px" }} className="mx-auto mb-3" >
-                <label for="exampleFormControlInput1" className="form-label">
+                <label htmlFor="exampleFormControlInput1" className="form-label">
                 Full Name
                 </label>
                 <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
             </div>
             <div style={{ width: "500px" }} className="mx-auto mb-3" >
-                <label for="exampleFormControlInput1" className="form-label">
+                <label htmlFor="exampleFormControlInput1" className="form-label">
                 Your Email
                 </label>
                 <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
             </div>
             <div style={{ width: "500px" }} className="mx-auto mb-3" >
-                <label for="exampleFormControlInput1" className="form-label">
+                <label htmlFor="exampleFormControlInput1" className="form-label">
                 Phone Number
                 </label>
                 <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
             </div>
             <div style={{ width: "500px" }} className="mx-auto mb-3" >
-                <label for="exampleFormControlInput1" className="form-label">
+                <label htmlFor="exampleFormControlInput1" className="form-label">
                 Company Name
                 </label>
                 <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
             </div>
-            <div style={{ width: "500px" }} class="mx-auto mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Your Message Here</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" ></textarea>
+            <div style={{ width: "500px" }} className="mx-auto mb-3">
+                <label htmlFor="exampleFormControlTextarea1" className="form-label">Your Message Here</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" ></textarea>
             </div>
           </div>
         </div>
