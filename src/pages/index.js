1e2c0b82162f6dc2db4ae4b9ac11ccc20924ea3f
ridho777 <ridho.artsys@gmail.com>
2025-05-21 18:30:51 +0700

    feat: Add about page and sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home";
 export { default as Portofolio } from "./Portofolio";
 export { default as Services } from "./Services";
+export { default as About } from "./About";
